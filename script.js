26a339933656435af686e6c9ba3db2e211bdf94a
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-06-17 14:17:08 +0530

    color changer

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("prateek")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);                 //target use karne se jispe click karo voh pata chal jata hai, for ex- grey, white, blue, yellow
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
